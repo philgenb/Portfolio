5ec3419be98f930f98ebcb324d1e842a779baf56
Phil <phil.gengenbach@online.de>
2024-09-14 17:15:51 +0200

    Replace aboutme icons through icon components

---
+++ src/assets/imageComponents/technologies/FlutterIcon.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const FlutterIcon = () => (
+    <svg width="21" height="25" viewBox="0 0 21 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path opacity="0.8" d="M20.1406 11.5396L13.4285 18.269L20.1406 24.9999H12.4677L9.59128 22.1161L5.75409 18.269L12.4677 11.5396H20.1406ZM12.4677 -0.00011011L-0.000222295 12.4999L3.83697 16.3469L20.1406 -0.00011011H12.4677Z" fill="#FF5D88"/>
+    </svg>
+
+)
--- src/components/TechnologyCard.js
+++ src/components/TechnologyCard.js
@@ -1,5 +1,14 @@
-export default function TechnologyCard({imgPath, bgColor}) {
-    return <div className={`flex w-[50px] h-[50px] items-center justify-center aspect-square rounded-lg`} style={{backgroundColor: bgColor}}>
-        <img src={imgPath} alt="TechnologyImage"/>
-    </div>
+export default function TechnologyCard({ imgPath, bgColor, iconComponent }) {
+    return (
+        <div
+            className="flex w-[50px] h-[50px] items-center justify-center aspect-square rounded-lg"
+            style={{ backgroundColor: bgColor }}
+        >
+            {imgPath ? (
+                <img src={imgPath} alt="Technology Image" />
+            ) : (
+                iconComponent
+            )}
+        </div>
+    );
 }
