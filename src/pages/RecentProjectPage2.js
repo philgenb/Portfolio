3f53214e652f9065947098963064079c60e2b475
Phil <phil.gengenbach@online.de>
2024-09-14 02:58:36 +0200

    Add relative positioning of my recent projects

--- src/pages/RecentProjectPage2.js
+++ src/pages/RecentProjectPage2.js
@@ -40,35 +40,44 @@ const RecentProjectPage2 = () => {
     return (
         <div id="recentProjects" className="flex flex-col h-fit sm:h-screen md:px-8 py-28 transition-all">
             <div className="flex flex-col gap-6 items-start sm:items-end xl:items-start transition-all">
-                <div className="flex min-w-fit flex-row justify-between w-full lg:pl-[25%] sm:items-end transition-all">
-                    <motion.div
-                        initial={'hidden'}
-                        animate={'visible'}
-                        transition={{ duration: 0.2, delay: 0.2}}
-                        variants={appearVariant}
-                        className="flex gap-4"
-                    >
-                        <div className="w-0.5 h-32 rounded-sm bg-sectionMarker"></div>
-                        <h1 className="font-satoshi text-4xl text-gray-900 whitespace-nowrap font-black transition-all">
-                            My recent<br />projects
-                        </h1>
-                    </motion.div>
-                    <ProjectTagSection className="self-end hidden sm:flex transition-all" tags={tags} activeTags={activeTags} />
+                <div className="flex flex-col sm:flex-row w-full lg:pl-[25%] transition-all">
+                    <div className="flex-grow"></div> {/* Platzhalter, um den rechten Bereich flexibel zu machen */}
+                    <ProjectTagSection
+                        className="hidden sm:flex transition-all self-end"
+                        tags={tags}
+                        activeTags={activeTags}
+                    />
                 </div>
-                <div className="flex flex-col sm:flex-row gap-12 sm:gap-4 md:gap-6 xl:gap-10 transition-all">
+                <div className="flex flex-col sm:flex-row gap-12 sm:gap-4 md:gap-6 xl:gap-14 transition-all w-full justify-center">
                     {tagList.map((tags, index) => (
-                        <ProjectCard
-                            key={index}
-                            title={`${titles[index]}`}
-                            description={`${descriptions[index]}`}
-                            tags={tags}
-                            animationDelay={0.1 * (index + 1)}
-                            classname=""
-                            onClick={() => handleCardClick(index + 1)}
-                            isSelected={selectedCard === index + 1}
-                            path={paths[index]}
-                            technologies={technologyList[index]}
-                        />
+                        <div className={`relative ${index === 1 ? 'flex' : ''}`}>
+                            <ProjectCard
+                                key={index}
+                                title={`${titles[index]}`}
+                                description={`${descriptions[index]}`}
+                                tags={tags}
+                                animationDelay={0.1 * (index + 1)}
+                                classname=""
+                                onClick={() => handleCardClick(index + 1)}
+                                isSelected={selectedCard === index + 1}
+                                path={paths[index]}
+                                technologies={technologyList[index]}
+                            />
+                            {index === 1 && (
+                                <motion.div
+                                    initial={'hidden'}
+                                    animate={'visible'}
+                                    transition={{ duration: 0.2, delay: 0.2}}
+                                    variants={appearVariant}
+                                    className="absolute top-[-10rem] left-1 flex gap-4"
+                                >
+                                    <div className="w-0.5 h-32 rounded-lg bg-sectionMarker"></div>
+                                    <h1 className="text-4xl text-gray-900 whitespace-nowrap font-black transition-all">
+                                        My recent<br />projects
+                                    </h1>
+                                </motion.div>
+                            )}
+                        </div>
                     ))}
                 </div>
             </div>
