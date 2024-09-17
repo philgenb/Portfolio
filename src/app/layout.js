a53f8756b27342b07f0b744fc92364e6d08ba201
Phil <phil.gengenbach@online.de>
2024-09-17 09:58:31 +0200

    Change page title

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,22 @@
-import './globals.css'
-import {Providers} from "./providers";
+import './globals.css';
+import { Providers } from './providers';
 
 export const metadata = {
-  title: 'Phils Portfolio',
-  description: 'Showcase of my recent work.',
-}
+    title: 'Phils Portfolio',
+    description: 'Showcase of my recent work.',
+};
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en" className="light">
-
-      <body>
-        <Providers>
-          {children}
-        </Providers>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en" className="light">
+        <head>
+            <link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,900,700,500,301,701,300,501,401,901,400,2&display=swap" rel="stylesheet" />
+            <meta name="description" content="Showcase of my recent work." />
+            <title>Phil G. | Portfolio</title>
+        </head>
+        <body>
+        <Providers>{children}</Providers>
+        </body>
+        </html>
+    );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -7,10 +7,10 @@ import AboutMePage from "@/pages/AboutMePage";
 export default function Home() {
   return (
       <>
-          <Head>
-              <link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,900,700,500,301,701,300,501,401,901,400,2&display=swap" rel="stylesheet"/>
-              <title>Phil G. | Portfolio</title>
-          </Head>
+          {/*<Head>*/}
+          {/*    <link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,900,700,500,301,701,300,501,401,901,400,2&display=swap" rel="stylesheet"/>*/}
+          {/*    <title>Phil G. | Portfolio</title>*/}
+          {/*</Head>*/}
           <main className="flex flex-col h-screen w-full items-center">
               <Navbar/>
               <LandingPage/>
